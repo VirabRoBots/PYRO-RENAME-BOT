ace44c8efe19212ae03374be3812c6d2b15c14d8
Virab_RoBots <114412453+VirabRoBots@users.noreply.github.com>
2022-10-04 06:36:31 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -46,7 +46,7 @@ async def start(client, message):
        photo=START_PIC,
        caption=f"""👋 Hai {user.mention} \n𝙸'𝚖 𝙰 𝚂𝚒𝚖𝚙𝚕𝚎 𝙵𝚒𝚕𝚎 𝚁𝚎𝚗𝚊𝚖𝚎+𝙵𝚒𝚕𝚎 𝚃𝚘 𝚅𝚒𝚍𝚎𝚘 𝙲𝚘𝚟𝚎𝚛𝚝𝚎𝚛 𝙱𝙾𝚃 𝚆𝚒𝚝𝚑 𝙿𝚎𝚛𝚖𝚊𝚗𝚎𝚗𝚝 𝚃𝚑𝚞𝚖𝚋𝚗𝚊𝚒𝚕 & 𝙲𝚞𝚜𝚝𝚘𝚖 𝙲𝚊𝚙𝚝𝚒𝚘𝚗 𝚂𝚞𝚙𝚙𝚘𝚛𝚝! """,
        reply_markup=InlineKeyboardMarkup( [[
-           InlineKeyboardButton(' Uᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ ', url='https://t.me/PYRO_BOTZ')
+           InlineKeyboardButton(' Uᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ ', url='https://t.me/Virab_Bot_Updates')
            ],[
            InlineKeyboardButton('Aʙᴏᴜᴛ', callback_data='about'),
            InlineKeyboardButton('Hᴇʟᴘ', callback_data='help')
@@ -90,14 +90,11 @@ async def cb_handler(client, query: CallbackQuery):
         await query.message.edit_text(
             text=f"""👋 Hai {query.from_user.mention} \n𝙸'𝚖 𝙰 𝚂𝚒𝚖𝚙𝚕𝚎 𝙵𝚒𝚕𝚎 𝚁𝚎𝚗𝚊𝚖𝚎+𝙵𝚒𝚕𝚎 𝚃𝚘 𝚅𝚒𝚍𝚎𝚘 𝙲𝚘𝚟𝚎𝚛𝚝𝚎𝚛 𝙱𝙾𝚃 𝚆𝚒𝚝𝚑 𝙿𝚎𝚛𝚖𝚊𝚗𝚎𝚗𝚝 𝚃𝚑𝚞𝚖𝚋𝚗𝚊𝚒𝚕 & 𝙲𝚞𝚜𝚝𝚘𝚖 𝙲𝚊𝚙𝚝𝚒𝚘𝚗 𝚂𝚞𝚙𝚙𝚘𝚛𝚝! """,
             reply_markup=InlineKeyboardMarkup( [[
-                InlineKeyboardButton("👼 𝙳𝙴𝚅𝚂 👼", callback_data='dev')                
-                ],[
-                InlineKeyboardButton('📢 𝚄𝙿𝙳𝙰𝚃𝙴𝚂', url='https://t.me/PYRO_BOTZ'),
-                InlineKeyboardButton('🍂 𝚂𝚄𝙿𝙿𝙾𝚁𝚃', url='https://t.me/PYRO_BOTZ_CHAT')
-                ],[
-                InlineKeyboardButton('🍃 𝙰𝙱𝙾𝚄𝚃', callback_data='about'),
-                InlineKeyboardButton('ℹ️ 𝙷𝙴𝙻𝙿', callback_data='help')
-                ]]
+                InlineKeyboardButton(' Uᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ ', url='https://t.me/Virab_Bot_Updates')
+           ],[
+           InlineKeyboardButton('Aʙᴏᴜᴛ', callback_data='about'),
+           InlineKeyboardButton('Hᴇʟᴘ', callback_data='help')
+           ]]
                 )
             )
         return
